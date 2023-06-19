0989101bbcb983de53340f7d260381babdb110df
Suporte <Suporte@SUPORTE>
2023-06-19 16:41:32 -0300

    Teste de inclusão da classe teste

---
+++ exerciciochatseguro/src/main/java/aprendendo/exerciciochatseguro/Teste.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendendo.exerciciochatseguro;
+
+/**
+ *
+ * @author Suporte
+ */
+public class Teste {
+    
+}
