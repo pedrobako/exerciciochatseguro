7a90beebd9ec4a311de6c88de4fbb09c751427a8
pedrobako <57959979+pedrobako@users.noreply.github.com>
2023-06-21 22:37:07 -0300

    Update Chat.java

--- exerciciochatseguro/src/main/java/aprendendo/exerciciochatseguro/Chat.java
+++ exerciciochatseguro/src/main/java/aprendendo/exerciciochatseguro/Chat.java
@@ -3,52 +3,38 @@ import java.net.UnknownHostException;
 
 
 public class Chat {
+    
+    public static void iniciaUsuario(String nomeUsuario, int porta, int posHoriz) throws UnknownHostException{
+        ChatBox chat;
+        chat = new ChatBox(nomeUsuario, porta);
+        chat.setLocation(posHoriz, 0);
+        chat.setVisible(true);
+    }
 
     public static void main(String[] args) throws UnknownHostException{        
-        int intPorta=0, portaFinal=0, intPos=0, posFinal=0;
-        String nomeUsuario = "default";
-		char idArg;
-
-        for (String arg : args){
-            arg = arg.toUpperCase().trim();
-            idArg = arg.charAt(0);
-
-            switch (idArg){
-                case 'U':
-                        nomeUsuario = arg.substring(1);
-                        break;
-                case 'L':
-                        intPos = Integer.parseInt(arg.substring(1));
-                        break;
-                case 'P':
-                        intPorta = Integer.parseInt(arg.substring(1));
-                        break;
-                default:
-                        break;
-            }
-        }
-        if (intPorta > 3000 && intPorta < 65536){
-            portaFinal = intPorta;
-        }
-        else{
-            portaFinal = 3001;
-        }
-        if (intPos >= 0 && intPos <= 870){
-            posFinal = intPos;
-        }
-        else{
-            posFinal = Math.abs(intPos - (intPos-870));
-        }
-		
-        nomeUsuario = "Alice";
-        ChatBox chat;
-        chat = new ChatBox(nomeUsuario, portaFinal);
-        chat.setLocation(posFinal, 0);
-        chat.setVisible(true);
+        String nomeUsuario;
+        char tipoOp;
         
-        ChatBox chatTeste;
-        chatTeste = new ChatBox("Bob", 3002);
-        chatTeste.setLocation(800, 0);
-        chatTeste.setVisible(true);
+        if (args.length > 0)
+            tipoOp = args[0].charAt(0);
+        else
+            tipoOp = 'V';
+        
+        //Operacao automatica com 2 usuarios
+        //Alice e Bob
+        if ((tipoOp == 'C') || (tipoOp == 'V')){
+            iniciaUsuario("Alice", 3001, 0);
+            iniciaUsuario("Bob", 3002, 800);
+        }
+        else if (tipoOp == 'A'){
+            iniciaUsuario("Alice", 3001, 400);
+        }
+        else if (tipoOp == 'B'){
+            iniciaUsuario("Bob", 3001, 400);
+        }
+        else if (tipoOp == 'U'){
+            nomeUsuario = args[0].substring(1);
+            iniciaUsuario(nomeUsuario, 3001, 400);
+        }
     }
 }
